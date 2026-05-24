67c4cd96f3bee534a201edad952346d36db5c41d
Sachin Pareta <66667087+pareta22@users.noreply.github.com>
2026-05-24 20:35:09 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Node Jenkins Demo</title>
+      </head>
+      <body style="font-family: Arial; text-align: center; margin-top: 80px;">
+        <h1>Hello Sachin!</h1>
+        <h2>This Node.js app is deployed by Jenkins + Docker Compose</h2>
+        <p>Version: 1.0</p>
+      </body>
+    </html>
+  `);
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'UP',
+    app: 'node-express-compose-jenkins'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
